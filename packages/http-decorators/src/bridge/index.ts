22a1424b56d399a64044fb5bfa3afcf9171199a2
paulohenriquevn <paulohenriquevn@gmail.com>
2026-06-09 16:26:35 -0300

    feat(http-decorators): NestJS MiddlewareConsumer builder + NestMiddleware interface
    T2.1: MiddlewareConsumer with apply().forRoutes().exclude() builder,
    NestMiddleware interface, functional middleware, route matching with
    wildcard, runMiddleware() executor. 10 new tests.

--- packages/http-decorators/src/bridge/index.ts
+++ packages/http-decorators/src/bridge/index.ts
@@ -2,6 +2,15 @@ export { resolveOrNew, type DiContainer } from './di-resolve.js'
 export { resolveDtoSchema } from './dto-zod.js'
 export { HttpDecoratorsConfigError } from './errors.js'
 export { runInterceptors, type Interceptor } from './interceptor-chain.js'
+export {
+  MiddlewareConsumerImpl,
+  runMiddleware,
+  middlewareMatchesPath,
+  type NestMiddleware,
+  type MiddlewareFn,
+  type ResolvedMiddleware,
+  type MiddlewareConfigProxy,
+} from './middleware-consumer.js'
 export { walkControllerMetadata, joinPath, type WalkResult } from './walk-metadata.js'
 export { registerControllers, type RouteRegistration } from './register-controllers.js'
 export { createDecoratorServer } from './create-server.js'
